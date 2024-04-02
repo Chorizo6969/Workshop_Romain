7f6b782acd19d600aa0bfb9a9cdedb96875a0b20
Chorizo <mateoledru05@gmail.com>
2024-04-02 19:11:09 +0200

    Des assets, une bombe et des tilemap

---
+++ Workshop Mario/Assets/Script/Ennemy.cs
@@ -0,0 +1,37 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Ennemy: MonoBehaviour
+{
+    public Transform _targetPosition;
+    public Transform _secondTarget;
+    public int speed = 5;
+    private SpriteRenderer _spriteRenderer;
+
+    private Transform target;
+
+    void Start()
+    {
+        target = _targetPosition;
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _spriteRenderer.flipX = false;
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        if (Vector3.Distance(transform.position, target.position) < 0.1f)
+        {
+            if (target == _targetPosition)
+            {
+                _spriteRenderer.flipX = true;
+                target = _secondTarget;
+            }
+            else
+            {
+                _spriteRenderer.flipX = false;
+                target = _targetPosition;
+            }
+        }
+    }
+}
