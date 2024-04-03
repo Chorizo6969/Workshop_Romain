5827088b899809bece15a7b67b7de51e61950a1c
Lucas Pécout <lucas.pecout@e-artsup.net>
2024-04-03 09:53:35 +0200

    update move

--- Workshop Mario/Assets/Script/Character/Move.cs
+++ Workshop Mario/Assets/Script/Character/Move.cs
@@ -90,16 +90,18 @@ public class Move : MonoBehaviour
 
     private void FixedUpdate()
     {
-
-        if (stickGaucheAxeX.x >= 0)
+        if (stickGaucheAxeX.x != 0)
         {
-            transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
-            transform.Translate(Time.deltaTime * vitese * new Vector2(stickGaucheAxeX.x, 0));
-        }
-        else if (stickGaucheAxeX.x <= 0)
-        {
-            transform.rotation = Quaternion.Euler(new Vector3(0, 180, 0));
-            transform.Translate(Time.deltaTime * vitese * new Vector2(-stickGaucheAxeX.x, 0));
+            if (stickGaucheAxeX.x > 0)
+            {
+                transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+                transform.Translate(Time.deltaTime * vitese * new Vector2(stickGaucheAxeX.x, 0));
+            }
+            else if (stickGaucheAxeX.x < 0)
+            {
+                transform.rotation = Quaternion.Euler(new Vector3(0, 180, 0));
+                transform.Translate(Time.deltaTime * vitese * new Vector2(-stickGaucheAxeX.x, 0));
+            }
         }
 
 
--- Workshop Mario/Assets/Script/eyesFollow.cs
+++ Workshop Mario/Assets/Script/eyesFollow.cs
@@ -13,8 +13,17 @@ public class eyesFollow : MonoBehaviour
 
     private void Update()
     {
-        Vector3 directon = transform.position + player.transform.position;
+        // Déterminez la direction du point cible par rapport à la position actuelle de l'objet
+        Vector2 directionToTarget = player.transform.position - this.transform.position;
 
+        // Calculez l'angle en radians à partir de la direction vers le point cible
+        float angle = Mathf.Atan2(directionToTarget.y, directionToTarget.x) * Mathf.Rad2Deg;
+        angle -= 90;
 
+        // Créez une rotation à partir de l'angle calculé
+        Quaternion newRotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+        // Appliquez la nouvelle rotation à l'objet
+        this.transform.rotation = newRotation;
     }
 }
